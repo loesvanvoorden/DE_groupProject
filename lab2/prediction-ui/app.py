18493918a0c8e31d4163940dd38038ff21969641
RoelandKramer999 <65554619+RoelandKramer999@users.noreply.github.com>
2024-10-29 14:37:54 +0100

    Update app.py

--- lab2/prediction-ui/app.py
+++ lab2/prediction-ui/app.py
@@ -48,4 +48,4 @@ def check_performance():
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
